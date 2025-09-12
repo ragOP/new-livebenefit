b7000f6f07d3b532d5109133ea2f32ee511078a6
farish-jamal <farishjamal98@gmail.com>
2025-09-12 17:05:47 +0530

    Comment out unused route paths in App component

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ const App = () => {
       <Routes>
       <Route path="/" element={<Home />} />
         <Route path="/engsf1" element={<Chatbotdq />} />
-        <Route path="/engsfdq" element={<Chatbotdq2 />} />
+        {/* <Route path="/engsfdq" element={<Chatbotdq2 />} />
         <Route path="/engsf2200" element={<ChatbotTwo />} />
         <Route path="/engsf1dup" element={<Chatbotdq2 />} />
         <Route path="/engsffast" element={<FastChat />} />
         <Route path="/engsf2200dup" element={<ChatbotFour />} />
-          <Route path="/engimg" element={<Raghib />} />
+          <Route path="/engimg" element={<Raghib />} /> */}
       </Routes>
     </Router>
   );
